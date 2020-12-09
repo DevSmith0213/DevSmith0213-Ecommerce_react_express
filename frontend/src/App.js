fbcfda2b82d4c1dd28cf867aeb205b36d8017c10
unknown <devsmith0213@gmail.com>
2020-12-09 23:17:31 +0800

    ShareSourceOnGit

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import data from './data'
+import Product from './components/Product'
+
+function App() {
+  return (
+    <div className="grid-container">
+        <header className="row">
+            <div>
+                <a href="/" className="brand">Amazona</a>
+            </div>
+            <div>
+                <a href="/cart">Cart</a>
+                <a href="/signin">Sign in</a>
+            </div>
+        </header>
+        <main>
+            <div>
+                <div className="row center">
+                    {
+                        data.products.map((product)=>(
+                            <Product key ={product._id} product={product} />
+                        ))
+                    }
+                    
+                </div>
+            </div>
+        </main>
+        <footer className="row center">All right reserved</footer>
+    </div>
+  );
+}
+
+export default App;
