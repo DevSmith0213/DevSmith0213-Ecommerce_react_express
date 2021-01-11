1de476ba795fb1dae1494d91e60ff515f8c7d486
devsmith0213 <devsmith0213@gmail.com>
2021-01-11 12:30:48 +0800

    SigninApiAdd

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,28 @@
 import express from 'express';
-import data from './data.js';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import userRouter from './routers/userRouter.js';
+import productRouter from './routers/productRouter.js';
+
+dotenv.config();
 
 const app = express();
-mongoose.connect('mongodb://localhost/amazon', {
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazon', {
     useNewUrlParser:true,
     useUnifiedTopology:true,
     useCreateIndex:true,
-})
-
-app.get('/api/products/:id', (req, res)=>{
-    const product = data.products.find((x) => x._id === req.params.id);
-    if(product){
-        res.send(product);
-    } else{
-        res.status(404).send({message:'Product not Found'})
-    }
 });
 
-app.get('/api/products', (req, res)=>{
-    res.send(data.products)
-});
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
 
 app.get('/', (req, res)=>{
     res.send('server is ready')
 });
-app.use('/api/users', userRouter);
+
 
 app.use((err, req, res, next) => {
     res.status(500).send({message:err.message});
